5838032eeb448e993255062d5deecf7e4ae0b97a
dewreeh <severussenap@gmail.com>
2025-04-08 02:41:54 +0300

    mapping ticket to string for kafka

--- newTicketsHandlerKafka/src/main/java/org/repin/service/KafkaConsumerService.java
+++ newTicketsHandlerKafka/src/main/java/org/repin/service/KafkaConsumerService.java
@@ -1,5 +1,6 @@
 package org.repin.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.repin.dto.Ticket;
 import org.repin.repository.TicketRepository;
@@ -11,11 +12,15 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class KafkaConsumerService {
     private final TicketRepository ticketRepository;
-    KafkaConsumerService(TicketRepository ticketRepository){
+    private final ObjectMapper objectMapper;
+    KafkaConsumerService(TicketRepository ticketRepository,
+                         ObjectMapper objectMapper){
         this.ticketRepository = ticketRepository;
+        this.objectMapper = objectMapper;
     }
-    @KafkaListener(topics = "bought-tickets")
-    public void consumeTicket(Ticket ticket){
-       log.info("Принят билет {}", ticket);
+    @KafkaListener(topics = "bought-tickets", groupId = "test")
+    public void consumeTicket(String message){
+       Ticket ticket = objectMapper.convertValue(message, Ticket.class);
+       log.info("Принят билет {}", message);
     }
 }
--- ticketService/src/main/java/org/repin/service/KafkaProducerService.java
+++ ticketService/src/main/java/org/repin/service/KafkaProducerService.java
@@ -1,5 +1,7 @@
 package org.repin.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.repin.model.Ticket;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -9,13 +11,22 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class KafkaProducerService{
 
-    private final KafkaTemplate<String, Ticket> kafkaTemplate;
+    private final KafkaTemplate<Object, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
-    KafkaProducerService(KafkaTemplate<String, Ticket> kafkaTemplate){
+    KafkaProducerService(KafkaTemplate<Object, String> kafkaTemplate,
+                         ObjectMapper objectMapper){
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
     public void sendTicketToKafka(Ticket ticket){
-            kafkaTemplate.send("bought-tickets", ticket);
+        try {
+            String str = objectMapper.writeValueAsString(ticket);
+            kafkaTemplate.send("bought-tickets", str);
+        } catch (JsonProcessingException ex){
+            log.error("Ошибка сериализации билета в JSON: {}", ticket.getId(), ex);
+            throw new RuntimeException(ex);
+        }
     }
 }
